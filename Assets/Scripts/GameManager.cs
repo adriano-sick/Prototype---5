c95da4418265c9a669eb265668b2efed30d17fbf
Adriano Siqueira <adriano_siqueira_silva@hotmail.com>
2020-04-29 09:52:06 -0300

    Init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate;
+
+    public TextMeshProUGUI scoreText;
+    public int score = 0;
+
+    public TextMeshProUGUI livesText;
+    public int lives = 10;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        livesText.text = "Lives: " + lives;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnRate = Random.Range(0, 3);       
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+            
+
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        //to score goes to new score count one by one insted just change the value, I make this coroutine
+        StartCoroutine(scoreStep(scoreToAdd));
+        
+    }
+
+    IEnumerator scoreStep(int scoreToAdd)
+    {
+        //if scoreToAdd is a positive number, this for loop will run "scoreToAdd" turns
+        for(int i = 0; i < scoreToAdd; i++)
+        {
+            score += 1;
+            scoreText.text = "Score: " + score;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        //if it's a negative number it will run "scoreToAdd" turns, but will decrease score value
+        for (int i = 0; i > scoreToAdd; i--)
+        {
+            score += -1;
+            scoreText.text = "Score: " + score;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+
+    public void UpdateLives(int scoreToAdd)
+    {        
+        lives += scoreToAdd;
+        livesText.text = "Lives: " + lives;
+    }
+
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float minSpeed = 12f;
+    public float maxSpeed = 16f;
+
+    public float xRange = 4f;
+    public float ySpawnPos = -6f;
+        
+    public float maxTorque = 10f;
+
+    private Rigidbody targetRb;
+
+    private GameManager gameManager;
+
+    public int pointValue = 0;
+    public int loseLiveValue = -1;
+
+    private AudioSource targetAudio;
+    public AudioClip clickSound;
+    private float destroyDelay = 0.1f;
+    public float SFXVol = 1f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomUpwardForce(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetAudio = GetComponent<AudioSource>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        gameManager.UpdateScore(pointValue);
+        targetAudio.PlayOneShot(clickSound, SFXVol);
+        
+        Destroy(gameObject, destroyDelay);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.UpdateLives(loseLiveValue);        
+        Destroy(gameObject);
+    }
+
+    float RandomTorque()
+    {
+        float randomTorque = Random.Range(0, maxTorque);
+        return randomTorque;
+    }
+
+    Vector3 RandomUpwardForce()
+    {
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        return Vector3.up * randomSpeed;
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        
+
+        float randomXPos = Random.Range(-xRange, xRange);
+
+        return new Vector3(randomXPos, ySpawnPos);
+    }
+
+    
+
+
+}
